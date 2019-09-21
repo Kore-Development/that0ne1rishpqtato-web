7af64b147e4446699def8dba7a7cadd294e8552d
Pqtato <29719362+Kore-Development@users.noreply.github.com>
2019-09-20 19:36:27 -0500

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,7 @@
       <div>
         <span class="title"><img src="../../public/logo.png" class="shadow"></span>
         <h3>Please select a category above.</h3>
+        <br/><a href="//salad242.that0ne1rishpqtato.gq"><img class="shadow" src="https://img.shields.io/badge/-salad242-blueviolet.svg" style="height:25px;"></a>
       </div>
     </div>
 </template>
